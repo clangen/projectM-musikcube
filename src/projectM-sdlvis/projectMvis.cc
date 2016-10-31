4ac95a6291dc7ad3172bdf635891293a92bb6d84
casey langen <casey.langen@gmail.com>
2016-10-30 23:55:30 -0700

    Replaced dummy SDL implementation with a vaguely working musikcube IPcmVisualizer implementation. Lots of work to be done, but the proof of concept works!

--- src/projectM-sdlvis/projectMvis.cc
+++ src/projectM-sdlvis/projectMvis.cc
@@ -20,71 +20,27 @@
  */
 
 #include <math.h>
-#include <projectM.hpp>
-#include "sdltoprojectM.h"
 #include <SDL_opengl.h>
 
-void renderLoop(projectM *pm, SDL_Window* screen) {
-    int i;
-    int x, y;
-    int index;
-    short pcm_data[2][512];
+#include <projectM.hpp>
 
-    while (true) {
-        projectMEvent evt;
-        projectMKeycode key;
-        projectMModifier mod;
+#include <sdk/IPcmVisualizer.h>
+#include <sdk/IPlugin.h>
 
-        /** Process SDL events */
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            /** Translate into projectM codes and process */
-            evt = sdl2pmEvent(event);
-            key = sdl2pmKeycode(event.key.keysym.sym);
-            mod = sdl2pmModifier(event.key.keysym.mod);
+#include "sdltoprojectM.h"
 
-            if (evt == PROJECTM_KEYDOWN) {
-                pm->key_handler( evt, key, mod );
-            }
-        }
+static projectM* pm = nullptr;
+static SDL_Window* screen = nullptr;
+static bool initialized = false;
 
-        /** Produce some fake PCM data to stuff into projectM */
-        for (i = 0 ; i < 512 ; i++) {
-            if (i % 2 == 0) {
-                pcm_data[0][i] = (float) (rand() / ((float) RAND_MAX ) * (pow(2,14)));
-                pcm_data[1][i] = (float) (rand() / ((float) RAND_MAX ) * (pow(2,14)));
-            }
-            else {
-                pcm_data[0][i] = (float) (rand() / ((float )RAND_MAX ) * (pow(2,14)));
-                pcm_data[1][i] = (float) (rand() / ((float) RAND_MAX ) * (pow(2,14)));
-            }
-
-            if (i % 2 == 1) {
-                pcm_data[0][i] = -pcm_data[0][i];
-                pcm_data[1][i] = -pcm_data[1][i];
-            }
-        }
-
-        pm->pcm()->addPCM16(pcm_data);
-        pm->renderFrame();
-
-        SDL_GL_SwapWindow(screen);
-    }
-
-    printf("Worker thread: Exiting\n");
-}
-
-
-int main(int argc, char *argv[]) {
-    projectM *pm = NULL;
-    SDL_Window *screen = NULL;
+static void threadProc(void* unused) {
     int fullscreen = 0;
     int width = 784;
     int height = 784;
     int flags = 0;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        return PROJECTM_ERROR;
+        return;
     }
 
     SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
@@ -110,13 +66,67 @@ int main(int argc, char *argv[]) {
     SDL_GL_CreateContext(screen);
 
     if (screen == NULL) {
-        return PROJECTM_ERROR;
+        return;
     }
 
     pm = new projectM("z:\\src\\projectM.inp");
-    pm->projectM_resetGL( width, height );
+    pm->projectM_resetGL(width, height);
 
-    renderLoop(pm, screen);
+    while (true) {
+        projectMEvent evt;
+        projectMKeycode key;
+        projectMModifier mod;
 
-    return PROJECTM_SUCCESS;
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            evt = sdl2pmEvent(event);
+            key = sdl2pmKeycode(event.key.keysym.sym);
+            mod = sdl2pmModifier(event.key.keysym.mod);
+
+            if (evt == PROJECTM_KEYDOWN) {
+                pm->key_handler(evt, key, mod);
+            }
+        }
+
+        pm->renderFrame();
+        SDL_GL_SwapWindow(screen);
+    }
+
+    printf("Worker thread: Exiting\n");
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+        _beginthread(threadProc, 0, 0);
+    }
+    return true;
+}
+
+class Plugin : public musik::core::IPlugin {
+public:
+    virtual void Destroy() { delete this; };
+    virtual const char* Name() { return "projectM IPcmVisualizer"; };
+    virtual const char* Version() { return "0.1"; };
+    virtual const char* Author() { return "clangen"; };
+};
+
+class Visualizer : public musik::core::audio::IPcmVisualizer {
+    public:
+        virtual void Destroy() {
+            delete this;
+        }
+
+        virtual void Write(musik::core::audio::IBuffer* buffer) {
+            if (pm) {
+                pm->pcm()->addPCMfloat(buffer->BufferPointer(), buffer->Samples());
+            }
+        }
+};
+
+extern "C" __declspec(dllexport) musik::core::IPlugin* GetPlugin() {
+    return new Plugin();
+}
+
+extern "C" __declspec(dllexport) musik::core::audio::IPcmVisualizer* GetPcmVisualizer() {
+    return new Visualizer();
 }
