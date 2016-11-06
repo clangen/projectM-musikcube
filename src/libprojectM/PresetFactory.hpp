1300787494b1bacfc65a36bfdc833505abc2f63f
casey langen <casey.langen@gmail.com>
2016-11-05 23:09:20 -0700

    Links in Linux now.

--- src/libprojectM/PresetFactory.hpp
+++ src/libprojectM/PresetFactory.hpp
@@ -10,33 +10,31 @@
 //
 //
 
-#include "Preset.hpp"
-#include <memory>
 
 #ifndef __PRESET_FACTORY_HPP
 #define __PRESET_FACTORY_HPP
 
+#include "Preset.hpp"
+#include <memory>
+
 class PresetFactory {
-
 public:
- static const std::string IDLE_PRESET_PROTOCOL;
- static std::string protocol(const std::string & url, std::string & path);
+    static const std::string IDLE_PRESET_PROTOCOL;
+    static std::string protocol(const std::string & url, std::string & path);
 
- inline PresetFactory() {}
+    PresetFactory() {}
+    virtual ~PresetFactory() {}
 
- inline virtual ~PresetFactory() {}
-
- /// Constructs a new preset given an url and optional meta data
- /// \param url a locational identifier referencing the preset
- /// \param name the preset name
- /// \param author the preset author
- /// \returns a valid preset object
- virtual std::shared_ptr<Preset> allocate(const std::string & url, const std::string & name=std::string(),
+    /// Constructs a new preset given an url and optional meta data
+    /// \param url a locational identifier referencing the preset
+    /// \param name the preset name
+    /// \param author the preset author
+    /// \returns a valid preset object
+    virtual std::shared_ptr<Preset> allocate(const std::string & url, const std::string & name=std::string(),
 	 const std::string & author=std::string()) = 0;
 
- /// Returns a space separated list of supported extensions
- virtual std::string supportedExtensions() const = 0;
-
+    /// Returns a space separated list of supported extensions
+    virtual std::string supportedExtensions() const = 0;
 };
 
 #endif
--- src/projectM-musikcube/projectMPcmVisualizerNix.cpp
+++ src/projectM-musikcube/projectMPcmVisualizerNix.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <fstream>
 #include <iostream>
