e0ceab8051f0d7e0ba4129efd052a032b7990f9c
casey langen <casey.langen@gmail.com>
2023-01-29 17:19:54 -0800

    Add script to clean cmake artifacts.

---
+++ script/clean-nix.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+make clean 2> /dev/null
+rm -rf bin
+find . -name CMakeCache.txt -delete
+find . -name CMakeFiles -type d -exec rm -rf "{}" \; 2> /dev/null
+rm -f Makefile
+rm -f *.cmake
+rm -rf _CPack_Packages 2> /dev/null
+rm CPack* 2> /dev/null
+rm install_manifest.txt 2> /dev/null
+#rm -rf src/libprojectM-* 2> /dev/null
